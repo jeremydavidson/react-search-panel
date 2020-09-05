b1a90b294f9a63ce28812f27234983eae6e32437
Jeremy Davidson <jeremyfdavidson@gmail.com>
2020-09-05 13:09:59 -0700

    Updates

---
+++ demo/build/precache-manifest.ef34b6d37ec9970216dd6e8ac88ed119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0057d2c58a18a611a764d56fc8e3d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7fb089e9356c284e227",
+    "url": "./static/css/main.1291c040.chunk.css"
+  },
+  {
+    "revision": "c2d02bd68bcb0106bd55",
+    "url": "./static/js/2.e8a97b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e8a97b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fb089e9356c284e227",
+    "url": "./static/js/main.abd2cf06.chunk.js"
+  },
+  {
+    "revision": "4f9e10d540bb139e4d79",
+    "url": "./static/js/runtime-main.b1e515ab.js"
+  }
+]);

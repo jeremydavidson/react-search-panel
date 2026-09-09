6e0e59f5850dcc5c6488d15b11cab548ccb08ebb
Jeremy Davidson <jeremyfdavidson@gmail.com>
2026-09-08 19:34:03 -0700

    fix: patched minimatch, qs, picomatch, webpack-dev-server, and once advisories

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
 import { createRequire } from "module";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
+const typescript = require("rollup-plugin-typescript2");
+const postcss = require("rollup-plugin-postcss");
+const commonjs = require("@rollup/plugin-commonjs");
 
 const input = "src/index.tsx";
 
 const plugins = [
-  commonjs(),
-  nodeResolve(),
-  postcss({
-    extract: false,
-    modules: true,
-    // use: ["sass"],
-  }),
   typescript({
     typescript: require("typescript"),
+    include: ["src/**/*.ts", "src/**/*.tsx"],
     tsconfigOverride: {
       compilerOptions: {
         skipLibCheck: true,
       },
     },
   }),
+  postcss({
+    extract: false,
+    modules: true,
+  }),
+  commonjs(),
+  nodeResolve(),
 ];
 
 export default [
